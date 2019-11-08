40a7c7403e12ca49afea5ad0b74126074e46581f
unknown <dawnspoerr@gmail.com>
2019-11-08 09:22:19 -0500

    Added functionality to automatically reply with the target's current healing assignments. only works if they are in the same raid.

--- ClassicHealAssignments.lua
+++ ClassicHealAssignments.lua
@@ -46,6 +46,7 @@ function RegisterEvents()
    -- Listen for changes in raid roster
    ClassicHealAssignments:RegisterEvent("CHANNEL_UI_UPDATE", "HandleChannelUpdate")
    ClassicHealAssignments:RegisterEvent("GROUP_ROSTER_UPDATE", "HandleRosterChange")
+   ClassicHealAssignments:RegisterEvent("CHAT_MSG_WHISPER", "ReplyWithAssignment")
 end
 
 
@@ -365,4 +366,25 @@ function GetRaidRoster()
    end
 
    return classes, roles
+end
+
+--listens for 'heal' and replies the target's current healing assignments if any
+--only replies if character is in raid
+function ClassicHealAssignments:ReplyWithAssignment(event, msg, character)
+	--chopping off server tag that comes with character to parse it more easily
+	local characterParse = string.sub(character, 0, (string.find(character, "-") - 1))
+	
+	if msg == "heal" and UnitInRaid(characterParse) then
+		local replyAssignment = {}
+		for target, healers in pairs(assignedHealers) do
+			for i, player in ipairs(healers) do
+				if player == characterParse then
+					table.insert(replyAssignment, target)
+					break
+				end
+			end
+		end
+
+		SendChatMessage("You are assigned to: " .. table.concat(replyAssignment, ", "), "WHISPER", nil, character)
+	end
 end
