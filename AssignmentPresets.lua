a4f21333e56ef692e2eb80ce896772e4d697a7cb
unknown <dawnspoerr@gmail.com>
2019-11-08 19:20:44 -0500

    added DebugPrint where needed.

--- AssignmentPresets.lua
+++ AssignmentPresets.lua
@@ -72,9 +72,7 @@ end
 
 -- Saves the current status of healers & their target
 function SavePreset(name)
-   if debug then
-      print("\n-----------\nSavePreset")
-   end
+   DebugPrint("\n-----------\nSavePreset")
 
    presetEditBoxText = name
    presetList[name] = {}
@@ -88,9 +86,7 @@ end
 
 --  Loads the state saved under the preset list
 function LoadPreset(name)
-   if debug then
-      print("\n-----------\nLoadPreset")
-   end
+   DebugPrint("\n-----------\nLoadPreset")
 
    presetEditBoxText = name
    selectedPreset = name
@@ -104,15 +100,12 @@ end
 
 -- deletes the preset with the current name in the edit text box
 function DeletePreset(name)
-   if debug then
-      print("\n-----------\nDeletePreset")
-   end
+   DebugPrint("\n-----------\nDeletePreset")
    presetList[name] = nil
    CleanupFrame()
    SetupFrameContainers()
    UpdateFrame()
 end
-
 
 -- TODO #14: make this generic and move into table.lua
 function CopyArray(array)
